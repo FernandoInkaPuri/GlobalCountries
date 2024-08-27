5e23603ed466f0465b1fa8bffe9a3195b7017d69
Fernando Puri <luiz.gomez@rebase.com.br>
2024-08-27 18:02:30 -0300

    Configure Apollo Client with lib apollo-client-nextjs

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1>Apollo Client configurado com Sucesso no Next!</h1>
+      </div>
+    </main>
+    );
+}
